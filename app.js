032ecf52c3b7d47bf2bf572d3f9845487717dbd2
anushreesonni <anushreesonni@users.noreply.github.com>
2016-04-23 21:04:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-/*This code is hidden to protect code privacy*/
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , MongoClient = require('mongodb').MongoClient;
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('routes', __dirname + '/routes');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+	
+	//res.send(data);	
+});
+
+
+app.get('/', routes.index);		//Enable get call on index
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
