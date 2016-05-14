e8b19ad3e3ef504c12208052fec759b35b88290f
anushreesonni <anushreesonni@users.noreply.github.com>
2016-05-14 13:07:22 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,23 @@
-/*This code is hidden to protect the code privacy*/
+var MongoClient = require('mongodb').MongoClient;
+var data = "xyz";
+MongoClient.connect('mongodb://@ds023108.mlab.com:23108/charts', function(err, db) {
+	  if (err) {
+	    throw err;
+	  }
+	  setInterval(function() {		  
+		  respond();
+		}, 5000);
+	  function respond(){
+		  db.collection('heartrae').find().sort({_id:-1}).toArray(function(err, result) { //Pick all data in the db
+			    if (err) {
+			      throw err;
+			    }
+			    data = result;
+			  });	  
+	  }	  
+	});
+
+exports.index = function send_again(req, res){	
+	  res.render('index', { title: "Heartrate vs Time", result:data});	  
+	};
+
